eaa55c4df5256b846c8082e6512fb62696b437c7
rafael blanco ruiz <rafa_de_cadiz@hotmail.com>
2015-10-08 22:01:29 +0200

    Update 1103_si.py

--- 1103_si.py
+++ 1103_si.py
@@ -2,9 +2,9 @@
 #-*- coding: utf-8 -*-
 def mayor(n1, n2):
 	if n1 > n2:
-		 salida = True	 
+		 salida = "se puede"	 
 	else:
-		salida = False	
+		salida = "igualmente se puede"
 	
 	return salida
 	
