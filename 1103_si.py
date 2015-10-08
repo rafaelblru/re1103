c29b6c21eb2b715d054ba92e691f4b678a7b490d
Rafael Blanco <rafa_de_cadiz@hotmail.com>
2015-10-08 21:49:04 +0200

    version 2

--- 1103_si.py
+++ 1103_si.py
@@ -2,12 +2,10 @@
 #-*- coding: utf-8 -*-
 def mayor(n1, n2):
 	if n1 > n2:
-		 salida = "a es mayor que b"
-		 salida = salida + "insisto a es mayor que b"
+		 salida = n1	 
 	else:
-		salida = "b es mayor que a"
-		salida +=" o igual"
-	print "esto lo va a pintar de todas maneras"
+		salida = n2		
+	
 	return salida
 	
 	
---
+++ 1103_si.py~
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+def mayor(n1, n2):
+	if n1 > n2:
+		 salida = "a es mayor que b"
+		 salida = salida + "insisto a es mayor que b"
+	else:
+		salida = "b es mayor que a"
+		salida +=" o igual"
+	print "esto lo va a pintar de todas maneras"
+	return salida
+	
+	
+
+
